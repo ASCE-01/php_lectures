6c8fd122cf31bfce50234ab53bda6ef73e1dfd43
levan tsiklauri <75841397+ASCE-01@users.noreply.github.com>
2023-03-25 00:26:08 +0400

    Create five.php

---
+++ levani/lecture2/classwork/five.php
@@ -0,0 +1,59 @@
+<style>
+        .container{
+            margin: auto;
+            width: 50%;
+            padding: 10px;
+            border: 1px black solid;
+        }
+        .error{
+            color: red;
+            font-weight: bold;
+        }
+</style>
+
+
+
+<?php
+
+$error = '';
+$name = "";
+$email = "";
+echo "<pre>";
+print_r($_POST);
+if (isset($_POST["send"])){
+    $name = htmlspecialchars(trim($_POST["user"]));
+    $email = htmlspecialchars(trim($_POST["email"]));
+    echo $name;
+    echo "<br>";
+    echo $email;
+    if (strlen($name) < 4) {
+        $error .= 'user is not valid!<br>';
+        $name_b = true;
+    }
+    if (strlen($email) < 4) {
+        $error .= 'email is not valid!<br>';
+        $email_b = true;
+    }
+    if (!isset($name_b) && !isset($email_b)){
+        $name = "";
+        $email = "";
+    }
+}
+
+?>
+
+<div class="container">
+    <h2>PHP form validation</h2>
+    <p>* required field</p>
+    <div class="error"><?=$error?></div>
+    <form action="" method="post">
+        <label for="user">Name:</label>
+        <input type="text" name="user" placeholder="user" value="<?=$name?>"> *
+        <br><br>
+        <label for="user">Email:</label>
+        <input type="text" name="email" placeholder="email" value="<?=$email?>"> *
+        <br><br>
+        <button name="send">Submit</button>
+    </form>
+    <h2>Your Input:</h2>
+</div>
