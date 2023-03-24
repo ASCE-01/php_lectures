aec31f451821a6ba22d1d605cf180a4d37bd959c
levan tsiklauri <75841397+ASCE-01@users.noreply.github.com>
2023-03-25 00:40:16 +0400

    Delete questions.php

--- levani/questions.php
+++
@@ -1,9 +0,0 @@
-<?php
-$questions = [
-    ["question" => "What is PHP?", "point" => 4],
-    ["question" => "What is Laravel?", "point" => 3],
-    ["question" => "What is Apache?", "point" => 4],
-    ["question" => "What is MySQL?", "point" => 4],
-    ["question" => "What is XAMPP?", "point" => 6]
-];
-?>
